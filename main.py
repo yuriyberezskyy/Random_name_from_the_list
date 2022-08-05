e80cbb21a1711eb3e2ba3d9e7c224c42a68bf8a1
Yuriy Berezskyy <54465428+yuriyberezskyy@users.noreply.github.com>
2022-08-05 00:57:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Split string method
+import random
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+random_name = names[random.randint(0,len(names)-1)]
+
+print(f"{random_name} is going to pay for the bill")
